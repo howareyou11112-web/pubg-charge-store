12233fa36c1411b9e97e0f817828663ec9791b1e
Ahmed <howareyou11112@gmail.com>
2026-03-17 06:20:52 +0300

    fix: update host and port for deployment

--- main.py
+++ main.py
@@ -66,4 +66,7 @@ def admin():
     '''
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # أضفنا host و port ليتناسب مع سيرفرات Render
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
